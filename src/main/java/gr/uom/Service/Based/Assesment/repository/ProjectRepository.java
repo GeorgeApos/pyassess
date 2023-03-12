1485913b91c42f74fbf68c426c37dc4ce03d202c
George David Apostolidis <iis20001@uom.edu.gr>
2023-03-12 18:43:16 +0200

    Fixes #46

--- src/main/java/gr/uom/Service/Based/Assesment/repository/ProjectRepository.java
+++ src/main/java/gr/uom/Service/Based/Assesment/repository/ProjectRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProjectRepository extends JpaRepository<Project, Long> {
-    Optional<Project> findByGitUrl(String gitUrl);
+    Optional<Project> findProjectByGitUrl(String gitUrl);
 
     Project findProjectBySHA(String sha);
 
